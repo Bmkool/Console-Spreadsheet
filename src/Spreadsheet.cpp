bb47680c324451da6a7c2ed32d606552df6721da
Brian Kachala <kachala.1@osu.edu>
2021-08-30 17:05:02 -0400

    Implementation and unit tests

--- src/Spreadsheet.cpp
+++ src/Spreadsheet.cpp
@@ -3,13 +3,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
 #include <algorithm>
 #include <optional>
+#include <set>
+#include <utility>
 
 static std::vector<std::string> tokenize(const std::string& str, const std::string& del);
 static std::optional<int> stringToInt(const std::string& str);
 static bool processFormula(unsigned int row, unsigned int col, std::vector<std::vector<std::string>>& spreadsheet);
+static std::pair<int,int> refToRowCol(const std::string& ref);
 
 bool fileToSpreadsheet(const char filename[], std::vector<std::vector<std::string>>& spreadsheet)
 {
@@ -38,9 +40,16 @@ bool processSpreadsheet(std::vector<std::vector<std::string>>& spreadsheet)
         for (unsigned int column = 0; column < spreadsheet[row].size(); column++) {
             // Check for valid integer. If valid then no processing needed
             if (std::nullopt == stringToInt(spreadsheet[row][column])) {
-                // Check if entry is a formula
+                // Check if no work is needed
+                if (spreadsheet[row][column].empty() ||
+                   (0 == spreadsheet[row][column].compare("#ERROR")) ||
+                   (0 == spreadsheet[row][column].compare("#NAN"))) {
+                       continue;
+                }
+
+                // Only valid entry left is a formula
                 if (spreadsheet[row][column][0] != '=') {
-                    std::cerr <<  "Input is not a valid integer or formula: " << spreadsheet[row][column] << std::endl;
+                    std::cerr <<  "Input is not a valid integer or formula: '" << spreadsheet[row][column] << "'" << std::endl;
                     return false;
                 }
 
@@ -82,28 +91,114 @@ static std::optional<int> stringToInt(const std::string& str) {
 
 static bool processFormula(unsigned int row, unsigned int col, std::vector<std::vector<std::string>>& spreadsheet) {
     auto tokens = tokenize(spreadsheet[row][col].substr(1,std::string::npos), "+-/*");
-    // Multiplication and Division subcalculations
-    auto it = tokens.begin();
-    while (it != tokens.end()) {
-        if (0 == it->compare("*")) {
-            // Do multiplication of prev and next token
-        } else if (0 == it->compare("/")) {
-            // Do division of prev and next token
+
+    // Set of coordinate pairs that are currently being processed
+    static std::set<std::pair<int,int>> callStack;
+
+    // Check if there is a circular reference
+    auto it = callStack.find(std::pair<int,int>(row,col));
+    if (callStack.end() != it) {
+        spreadsheet[row][col] = "#ERROR";
+        callStack.erase({row, col});
+        return true;
+    }
+
+    // Add current entry to processing stack
+    callStack.insert({row, col});
+
+    // Should always be an odd number of tokens [int(,op,int)+)
+    if ((tokens.size() & 1) == 0) {
+        std::cerr << "Invalid Formula: '" << spreadsheet[row][col] << "'" << std::endl; 
+        callStack.erase({row, col});;
+        return false;
+    }
+
+    // Recursive process to determine the final value of the formula.  
+    auto formulaToInt = [&] (std::optional<int>& entry, int pos) -> std::optional<bool> { 
+        auto coord = refToRowCol(tokens[pos]);
+        // Make sure references exist and are valid
+        if (coord.first < 0 || coord.second < 0) {
+            if (0 == spreadsheet[row][col].compare("#NAN")) {
+                spreadsheet[row][col] = "#NAN";
+                callStack.erase({row, col});
+                return true;  
+            }
+            std::cerr << "Invalid Formula: '" << spreadsheet[row][col] << "'" << std::endl; 
+            callStack.erase({row, col});;
+            return false;
+        } else if (coord.first >= static_cast<int>(spreadsheet.size()) || 
+                    coord.second >= static_cast<int>(spreadsheet[coord.first].size()) || 
+                    spreadsheet[coord.first][coord.second].empty())
+        {
+            spreadsheet[row][col] = "#NAN";
+            callStack.erase({row, col});
+            return true;
+        }
+        // Evaluate reference
+        auto tmp = stringToInt(spreadsheet[coord.first][coord.second]);
+        if (tmp.has_value()) {
+            entry.emplace(tmp.value());
         } else {
-            it++;
+            // Recursively evaluate referenced cell
+            if (!processFormula(coord.first, coord.second, spreadsheet)) {
+                callStack.erase({row, col});
+                return false;
+            }
+            // Re-evaluate entry after reference processing. Propogate referenced cell ERRORs and NANs
+            entry = stringToInt(spreadsheet[coord.first][coord.second]);
+            if (!entry.has_value()) {
+                spreadsheet[row][col] = spreadsheet[coord.first][coord.second];
+                callStack.erase({row, col});
+                return true;
+            }
+        }
+        return std::nullopt;
+    };
+
+    int result = 0;
+    std::optional<int> last = stringToInt(tokens[0]);
+    if (!last.has_value()) {
+        auto rc = formulaToInt(last, 0);
+        if (rc.has_value()) {
+            return rc.value();
         }
     }
 
-    // Addition and Subtraction subcalculations
-    while (it != tokens.end()) {
-        if (0 == it->compare("+")) {
-            // Do multiplication of prev and next token
-        } else if (0 == it->compare("-")) {
-            // Do division of prev and next token
+    // iterate over each operation
+    for (unsigned int i = 1; i + 1 < tokens.size(); i+=2) {
+        // Make sure next token is valid and evaluate the formula if necessary
+        auto next = stringToInt(tokens[i+1]);
+        if (!next.has_value()) {
+            auto rc = formulaToInt(next, i+1);
+            if (rc.has_value()) {
+                return rc.value();
+            }
+        }
+
+        // Perform multiplication and division operations for the current "last" value as those take 
+        // precedence. For addition and subtraction add the last value immediately to the result and update
+        // the "last" value with the next one
+        if (0 == tokens[i].compare("*")) {
+            last.emplace(last.value() * next.value());
+        } else if (0 == tokens[i].compare("/")) {
+            last.emplace(last.value() / next.value());
+        } else if (0 == tokens[i].compare("+")) {
+            result += last.value();
+            last.emplace(next.value());
+        } else if (0 == tokens[i].compare("-")) {
+            result += last.value();
+            last.emplace(-next.value());
         } else {
-            it++;
+            std::cerr << "Invalid Formula: '" << spreadsheet[row][col] << "'" << std::endl; 
+            callStack.erase({row, col});;
+            return false;       
         }
     }
+    // Add the last remaining token
+    result += last.value();
+
+    spreadsheet[row][col] = std::to_string(result);
+    callStack.erase({row, col});
     return true;
 
 }
@@ -111,18 +206,38 @@ static bool processFormula(unsigned int row, unsigned int col, std::vector<std::
 // Return strings separated by any of the passed in delimiters. The delimiters are included as individual tokens
 static std::vector<std::string> tokenize(const std::string& str, const std::string& del) {
     std::vector<std::string> tokens;
-    int prev = 0;
-    int pos = str.find_first_of(del);
+    size_t prev = 0;
+    size_t pos = str.find_first_of(del);
 
-    while (pos != -1) {
-        tokens.push_back(str.substr(prev,pos));
+    while (pos - prev != std::string::npos) {
+        tokens.push_back(str.substr(prev,pos-prev));
         tokens.push_back(std::string{str[pos]});
         prev = pos + 1;
-        pos = str.substr(prev, std::string::npos).find_first_of(del);
+        pos = prev + str.substr(prev, std::string::npos).find_first_of(del);
     }
 
-    // Push back last token
-    tokens.push_back(str.substr(prev, std::string::npos));
+
+    // Push back last token if not delimiter
+    if (prev < str.length()) {
+        tokens.push_back(str.substr(prev, std::string::npos));
+    }
 
     return tokens;
+}
+
+// Returns <row,col> index pair for the entry that is references, else <-1,-1> if invalid
+static std::pair<int,int> refToRowCol(const std::string& ref) {
+    size_t pos = 0, col = 0;
+    while (pos < ref.length() && ref[pos] >= 'A' && ref[pos] <= 'Z') {
+        col = col*26 + ref[pos++] - 'A' + 1;
+    }
+
+    auto row = stringToInt(ref.substr(pos, std::string::npos));
+
+    if (col > 0 && row.has_value()) {
+        return std::pair<int,int>(row.value() -1 ,col -1);
+    } else {
+        return std::pair<int,int>(-1, -1);
+    }
+
 }
--- unit-tests/Tests.cpp
+++ unit-tests/Tests.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 
 #include "../src/Spreadsheet.hpp"
+#include "../src/Spreadsheet.cpp"
 
 constexpr auto TEST_FILENAME = "TEST_FILE.txt";
 
@@ -56,13 +57,13 @@ TEST_F(FileToSpreadsheetTests, SingleRowMultipleColumnSS) {
 }
 
 TEST_F(FileToSpreadsheetTests, MultipleRowMultipleColumnSS) {
-    output << "1\t5\n6\n\t=A1+A2";
+    output << "1\t5\n6\n\t\t=A1+A2";
     output.close();
     std::vector<std::vector<std::string>> exp
     {
         {"1", "5"},
         {"6"},
-        {"", "=A1+A2"}
+        {"", "", "=A1+A2"}
     };
     EXPECT_TRUE(fileToSpreadsheet(TEST_FILENAME, ss));
     EXPECT_EQ(exp,ss);
@@ -109,16 +110,241 @@ TEST(ProcessSpreadsheetTests, InvalidInputIntOverflow) {
     ASSERT_FALSE(processSpreadsheet(tst));
 }
 
-TEST(ProcessSpreadsheetTests, FormulaNoReference) {
+TEST(ProcessSpreadsheetTests, FormulaNoReferenceAddSub) {
     const std::vector<std::vector<std::string>> exp
     {
-        {"1", "2"},
+        {"1", "8"},
         {"2"}
     };
     std::vector<std::vector<std::string>> tst
     {
-        {"1", "=5-3"},
+        {"1", "=5-3+1+5"},
         {"2"}
+    };
+    ASSERT_TRUE(processSpreadsheet(tst));
+    EXPECT_EQ(exp,tst);
+}
+
+TEST(ProcessSpreadsheetTests, FormulaNoReferenceAddSubMultDiv) {
+    const std::vector<std::vector<std::string>> exp
+    {
+        {"1", "-4"},
+        {"2"}
+    };
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "=5-3*2*2+9/3"},
+        {"2"}
+    };
+    ASSERT_TRUE(processSpreadsheet(tst));
+    EXPECT_EQ(exp,tst);
+}
+
+TEST(ProcessSpreadsheetTests, FormulaInvalidTokenOperatorFirst) {
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "=-*3"},
+        {"2"}
+    };
+    ASSERT_FALSE(processSpreadsheet(tst));
+}
+
+TEST(ProcessSpreadsheetTests, FormulaInvalidTokenOperatorLast) {
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "=3*6*"},
+        {"2"}
+    };
+    ASSERT_FALSE(processSpreadsheet(tst));
+}
+
+TEST(ReferenceToRowCol, InvalidNoCol) {
+    auto pact = refToRowCol("1");
+    auto pexp =std::pair<int,int>(-1,-1);
+    EXPECT_EQ(pexp, pact);
+}
+
+TEST(ReferenceToRowCol, InvalidNoRow) {
+    auto pact = refToRowCol("A");
+    auto pexp =std::pair<int,int>(-1,-1);
+    EXPECT_EQ(pexp, pact);
+}
+
+TEST(ReferenceToRowCol, InvalidSymbolCol) {
+    auto pact = refToRowCol(">1");
+    auto pexp =std::pair<int,int>(-1,-1);
+    EXPECT_EQ(pexp, pact);
+}
+
+TEST(ReferenceToRowCol, InvalidSymbolRow) {
+    auto pact = refToRowCol("G,");
+    auto pexp =std::pair<int,int>(-1,-1);
+    EXPECT_EQ(pexp, pact);
+}
+
+TEST(ReferenceToRowCol, ValidRowColA1) {
+    auto pact = refToRowCol("A1");
+    auto pexp =std::pair<int,int>(0,0);
+    EXPECT_EQ(pexp, pact);
+}
+
+TEST(ReferenceToRowCol, ValidRowColZ9) {
+    auto pact = refToRowCol("Z9");
+    auto pexp =std::pair<int,int>(8,25);
+    EXPECT_EQ(pexp, pact);
+}
+
+TEST(ReferenceToRowCol, ValidRowColBA11) {
+    auto pact = refToRowCol("BA11");
+    auto pexp =std::pair<int,int>(10,52);
+    EXPECT_EQ(pexp, pact);
+}
+
+TEST(ReferenceToRowCol, ValidARowColAAA111) {
+    auto pact = refToRowCol("AAA111");
+    auto pexp =std::pair<int,int>(110,702);
+    EXPECT_EQ(pexp, pact);
+}
+
+TEST(ProcessSpreadsheetTests, FormulaReferenceOtherBox) {
+    const std::vector<std::vector<std::string>> exp
+    {
+        {"1", "1"},
+        {"2"}
+    };
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "=A1"},
+        {"2"}
+    };
+    ASSERT_TRUE(processSpreadsheet(tst));
+    EXPECT_EQ(exp,tst);
+}
+
+TEST(ProcessSpreadsheetTests, FormulaReferenceSelf) {
+    const std::vector<std::vector<std::string>> exp
+    {
+        {"1", "#ERROR"},
+        {"2"}
+    };
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "=B1"},
+        {"2"}
+    };
+    ASSERT_TRUE(processSpreadsheet(tst));
+    EXPECT_EQ(exp,tst);
+}
+
+TEST(ProcessSpreadsheetTests, FormulaThreeWayCircular) {
+    const std::vector<std::vector<std::string>> exp
+    {
+        {"1", "#ERROR"},
+        {"#ERROR", "#ERROR"}
+    };
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "=A2"},
+        {"=B2", "=B1"}
+    };
+    ASSERT_TRUE(processSpreadsheet(tst));
+    EXPECT_EQ(exp,tst);
+}
+
+TEST(ProcessSpreadsheetTests, FormulaNAN) {
+    const std::vector<std::vector<std::string>> exp
+    {
+        {"1", "#NAN", "#NAN"},
+        {"2"}
+    };
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "=B2", "=D1"},
+        {"2"}
+    };
+    ASSERT_TRUE(processSpreadsheet(tst));
+    EXPECT_EQ(exp,tst);
+}
+
+TEST(ProcessSpreadsheetTests, FormulaNANPropogation) {
+    const std::vector<std::vector<std::string>> exp
+    {
+        {"1", "#NAN", "#NAN"},
+        {"2"}
+    };
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "=B2", "=B1+3"},
+        {"2"}
+    };
+    ASSERT_TRUE(processSpreadsheet(tst));
+    EXPECT_EQ(exp,tst);
+}
+
+
+TEST(ExampleTests, Sample1) {
+    const std::vector<std::vector<std::string>> exp
+    {
+        {"1", "2"},
+        {"", "3"},
+        {"","","6"}
+    };
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "2"},
+        {"", "3"},
+        {"","","=A1+B1+B2"}
+    };
+    ASSERT_TRUE(processSpreadsheet(tst));
+    EXPECT_EQ(exp,tst);
+}
+
+TEST(ExampleTests, Sample2) {
+    const std::vector<std::vector<std::string>> exp
+    {
+        {"1", "2"},
+        {"", "3"},
+        {"","","#NAN"}
+    };
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "2"},
+        {"", "3"},
+        {"","","=A1+B1+A2"}
+    };
+    ASSERT_TRUE(processSpreadsheet(tst));
+    EXPECT_EQ(exp,tst);
+}
+
+TEST(ExampleTests, Sample3) {
+    const std::vector<std::vector<std::string>> exp
+    {
+        {"1", "2"},
+        {"", "3"},
+        {"","","0"}
+    };
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "=A1*2"},
+        {"", "=A1+B1"},
+        {"","","=A1+B1-B2"}
+    };
+    ASSERT_TRUE(processSpreadsheet(tst));
+    EXPECT_EQ(exp,tst);
+}
+
+TEST(ExampleTests, Sample4) {
+    const std::vector<std::vector<std::string>> exp
+    {
+        {"1", "#ERROR"},
+        {"", "3"},
+        {"","","#ERROR"}
+    };
+    std::vector<std::vector<std::string>> tst
+    {
+        {"1", "=B2-C3"},
+        {"", "3"},
+        {"","","=A1+B1"}
     };
     ASSERT_TRUE(processSpreadsheet(tst));
     EXPECT_EQ(exp,tst);
